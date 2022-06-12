9b4b8b1d43b6876b1f362432eee15b71a0f4f352
Dan Dan <Dan94g@mail.ru>
2022-06-13 03:23:37 +0500

    move

---
+++ Models/Messages/RewardRedeemedUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Twitcher.PubSub.Models.Messages
+{
+    public class RewardRedeemedUserModel
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("login")]
+        public string? Login { get; set; }
+
+        [JsonPropertyName("display_name")]
+        public string? DisplayName { get; set; }
+
+    }
+}
